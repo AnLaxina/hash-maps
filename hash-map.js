15278ace8bc054084df1c74de9e8a718231d8f4e
AnLaxina <anilovlaxina@gmail.com>
2025-07-02 12:20:06 -0700

    Add files for the HashMap class & the main file for running/testing

---
+++ hash-map.js
@@ -0,0 +1,5 @@
+export default class HashMap {
+    constructor() {
+        console.log("I've been created!");
+    }
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+import HashMap from "./hash-map.js";
+
+const hashMap = new HashMap();
