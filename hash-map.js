756f23631fdfbf5487205b7a8aae6b8445aa2e35
AnLaxina <anilovlaxina@gmail.com>
2025-07-05 13:08:05 -0700

    Add proper implementation for the set() method

--- hash-map.js
+++ hash-map.js
@@ -1,5 +1,5 @@
 export default class HashMap {
-    
+
     constructor(numberOfEntries = 0) {
         // For now, I'll just choose 16 for the capacity!
         this.capacity = 16;
@@ -12,31 +12,44 @@ export default class HashMap {
         let hashCode = 0;
         let strKey = key.toString();
         const primeNumber = 31;
-        for(let i = 0; i < strKey.length; i++) {
+        for (let i = 0; i < strKey.length; i++) {
             // Generate a hash code by multiplying and adding char codes of the stringified key
             hashCode = primeNumber * hashCode + strKey.charCodeAt(i);
         }
 
-        return this.#ifExceededMaxInteger(hashCode) ? hashCode % Number.MAX_SAFE_INTEGER: hashCode; 
+        return this.#ifExceededMaxInteger(hashCode) ? hashCode % Number.MAX_SAFE_INTEGER : hashCode;
     }
 
     set(key, value) {
         const hashCode = this.hash(key);
         const bucketIndex = hashCode % this.capacity;
-        if(this.buckets[bucketIndex] === undefined) {
-            const bucket = [key, value];
-            this.buckets[bucketIndex] = bucket;
-            console.log(`The bucketIndex is: ${bucketIndex} and the bucket is: ${bucket}`);
+
+        if (this.buckets[bucketIndex] === undefined) {
+            this.buckets[bucketIndex] = [];
         }
 
+        const bucket = this.buckets[bucketIndex];
+
+        // Check if key already exists and update if so
+        for (let pair of bucket) {
+            const [storedKey] = pair;
+
+            if (storedKey === key) {
+                pair[1] = value;
+                return;
+            }
+        }
+
+        bucket.push([key, value]);
+        this.size++;
     }
 
+
     #ifExceededMaxInteger(hashCode) {
-        if(hashCode > Number.MAX_SAFE_INTEGER) {
+        if (hashCode > Number.MAX_SAFE_INTEGER) {
             // Use modulo to avoid an incredibly large hashcode
             return true;
         }
-
         return false;
     }
 
--- main.js
+++ main.js
@@ -2,7 +2,10 @@ import HashMap from "./hash-map.js";
 
 const hashMap = new HashMap();
 
-console.log(`1. Rama's hash: ${hashMap.hash("Rama")}`);
-console.log(`2. Sita's hash: ${hashMap.hash("Sita")}`);
+console.log(`1. chicken's hash: ${hashMap.hash("chicken")}`);
+console.log(`2. chicken's hash: ${hashMap.hash("chicken")}`);
 
 hashMap.set("chicken", "nugget");
+hashMap.set("chicken", "sauce");
+
+hashMap.set("cheap trick", "doggy");
