8a799e4fbf0de6b993f07c42ccb7eafce30bcda5
AnLaxina <anilovlaxina@gmail.com>
2025-07-04 22:16:53 -0700

    Add proper hash function for hashmap

--- hash-map.js
+++ hash-map.js
@@ -1,5 +1,29 @@
 export default class HashMap {
-    constructor() {
-        console.log("I've been created!");
+    
+    constructor(numberOfEntries = 0) {
+        // For now, I'll just choose 16 for the capacity!
+        this.capacity = 16;
+        this.loadFactor = numberOfEntries / this.capacity;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+        let strKey = key.toString();
+        const primeNumber = 31;
+        for(let i = 0; i < strKey.length; i++) {
+            // Generate a hash code by multiplying and adding char codes of the stringified key
+            hashCode = primeNumber * hashCode + strKey.charCodeAt(i);
+        }
+
+        return this.#ifExceededMaxInteger(hashCode) ? hashCode % Number.MAX_SAFE_INTEGER: hashCode; 
+    }
+
+    #ifExceededMaxInteger(hashCode) {
+        if(hashCode > Number.MAX_SAFE_INTEGER) {
+            // Use modulo to avoid an incredibly large hashcode
+            return true;
+        }
+        
+        return false;
     }
 }
--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import HashMap from "./hash-map.js";
 
 const hashMap = new HashMap();
+const coolStringToTest = "Hello! I'm sooo cool!";
+const coolNum = 100000000000000000000;
+
+// console.log(`1. coolStringToTest's hash: ${hashMap.hash(coolStringToTest)}`);
+// console.log(`2. coolStringToTest's hash: ${hashMap.hash(coolStringToTest)}`);
+// console.log(`3. coolStringToTest's hash: ${hashMap.hash(coolStringToTest)}`);
+
+// console.log(`coolNum is: ${hashMap.hash(coolNum.toString())}`);
+console.log(`coolNum is: ${hashMap.hash(coolNum)}`);
