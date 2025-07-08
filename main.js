af0e30657f2b443ea7f974eb73aa142ed8fba4d8
AnLaxina <anilovlaxina@gmail.com>
2025-07-08 09:42:38 -0700

    Add one more test case to check if the hash map still works properly

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import HashMap from "./hash-map.js";
 
-const hashMap = new HashMap();
+const test = new HashMap();
 
-console.log(`1. chicken's hash: ${hashMap.hash("chicken")}`);
-console.log(`2. chicken's hash: ${hashMap.hash("chicken")}`);
+ test.set('apple', 'red')
+ test.set('banana', 'yellow')
+ test.set('carrot', 'orange')
+ test.set('dog', 'brown')
+ test.set('elephant', 'gray')
+ test.set('frog', 'green')
+ test.set('grape', 'purple')
+ test.set('hat', 'black')
+ test.set('ice cream', 'white')
+ test.set('jacket', 'blue')
+ test.set('kite', 'pink')
+ test.set('lion', 'golden')
 
-console.log("Adding chicken, cow, and dog:");
-hashMap.set("chicken", "nugget");
-hashMap.set("cow", "sauce");
-hashMap.set("dog", "sauce");
+ console.log("The intial entries are: ", test.entries());
 
-console.log(`The entries of the hashmap are:`, hashMap.entries());
+ test.set('apple', 'blue');
+ test.set('lion', 'leo');
+
+ console.log('Now the new entries are: ', test.entries());
 
